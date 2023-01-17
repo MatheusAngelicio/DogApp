2a78bfa367d98e93fb74aa6d10f0c45ec6c02c8d
Matheus Angelicio <matheus.angelicio00@gmail.com>
2023-01-17 18:58:52 -0300

    add dogs src

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    Container(
+      color: Colors.white,
+      child: Center(
+        child: _text(),
+      ),
+    );
+  }
+
+  _text() {
+    return Text(
+      "Hello World",
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.blue,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        decoration: TextDecoration.underline,
+        decorationColor: Colors.red,
+        decorationStyle: TextDecorationStyle.wavy,
+      ),
+    );
+  }
+}
